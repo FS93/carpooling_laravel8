ba5c3fea460c714e9e17dcc9fe091572059697ea
flck <florian.schroeck@stud.htwk-leipzig.de>
2022-07-07 10:38:46 +0200

    Schriftgröße & Tabelle in searchResult

--- resources/views/searchResults.blade.php
+++ resources/views/searchResults.blade.php
@@ -2,9 +2,39 @@
 
 @section('content')
     <div class="d-flex justify-content-center display-2">
-        Destination: {{ $departure }} <br>
-        Departure: {{ $destination }} <br>
-        Start Date: {{ $startDate }}
+        Destination: {{ $departure }}, Departure: {{ $destination }}, Start Date: {{ $startDate }}
+    </div>
+
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 
 
